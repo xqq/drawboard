382d959b929b481db648102d5edec4b37005bb37
xqq <xqq@xqq.im>
2019-12-28 02:33:22 +0900

    socket/TcpListener: WIN32 -> _WIN32

--- socket/tcp_listener.cpp
+++ socket/tcp_listener.cpp
@@ -4,7 +4,7 @@
 
 #include "tcp_listener.hpp"
 
-#ifndef WIN32
+#ifndef _WIN32
     #include "tcp_listener_posix.hpp"
 #else
     #include "tcp_listener_winsock.hpp"
@@ -13,7 +13,7 @@
 TcpListener* TcpListener::Create(OnAcceptedCallback on_accept, OnErrorCallback on_error) {
     TcpListener* listener = nullptr;
 
-#ifndef WIN32
+#ifndef _WIN32
     listener = new TcpListenerPosix(on_accept, on_error);
 #else
     listener = new TcpListenerWinsock(on_accept, on_error);
