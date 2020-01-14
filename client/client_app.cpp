784f395ff7f56e889483028ebbd194e4a6d3681e
xqq <xqq@xqq.im>
2020-01-14 12:04:49 +0900

    ClientApp: Apply AbstractCanvas test code

--- client/client_app.cpp
+++ client/client_app.cpp
@@ -27,18 +27,53 @@ int ClientApp::Run() {
         return 1;
     }
 
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 1280, 720);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    bool left_mouse_down = false;
+    uint32_t sequence_id = 0;
 
-    SDL_Event event;
-    while (SDL_WaitEvent(&event)) {
+    canvas_.InitPixelBuffer(1280, 720);
+
+    while (true) {
+        SDL_Event event;
+        SDL_PollEvent(&event);
         if (event.type == SDL_QUIT) {
             break;
+        } else {
+            switch (event.type) {
+                case SDL_MOUSEBUTTONUP:
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        left_mouse_down = false;
+                        canvas_.EndDraw(0, sequence_id);
+                    }
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        sequence_id++;
+                        canvas_.BeginDraw(0, sequence_id, 0);
+                        left_mouse_down = true;
+                    }
+                case SDL_MOUSEMOTION:
+                    if (left_mouse_down) {
+                        int mouseX = event.motion.x;
+                        int mouseY = event.motion.y;
+                        canvas_.DrawPoint(0, sequence_id, Point(mouseX, mouseY));
+                    }
+                    break;
+            }
         }
+
+        canvas_.Render(1280, 720);
+
+        SDL_UpdateTexture(texture, NULL, canvas_.GetPixelBuffer(), 1280 * sizeof(uint32_t));
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
--- client/client_app.hpp
+++ client/client_app.hpp
@@ -5,6 +5,7 @@
 #ifndef DRAWBOARD_CLIENT_APP_HPP
 #define DRAWBOARD_CLIENT_APP_HPP
 
+#include <common/abstract_canvas.hpp>
 #include "sdl_include.hpp"
 
 class ClientApp {
@@ -13,6 +14,8 @@ public:
 private:
     SDL_Window* window = nullptr;
     SDL_Surface* screen = nullptr;
+    SDL_Texture* texture = nullptr;
+    AbstractCanvas canvas_;
 };
 
 
